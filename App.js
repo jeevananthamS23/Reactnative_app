081e42836bfa2bce3199b55dd13f9b2e992230ed
jeevananthamS23 <jeevanantham.s2023ai-ds@sece.ac.in>
2026-01-25 23:25:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>i am jeevanantham S</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    color:'black',
+    justifyContent: 'center',
+  },
+});
