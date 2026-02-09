67757d517315f9ecae31f88c3457616cc205f0be
jeevananthamS23 <jeevanantham.s2023ai-ds@sece.ac.in>
2026-02-09 10:15:43 +0530

    goal list

--- App.js
+++ App.js
@@ -10,93 +10,63 @@ export default function App() {
   }
 
   function setgoal(){
-    setgoals(currentgoals=>[...currentgoals,enteredvalue]);
+    setgoals(currentgoals=>[...currentgoals,{text:enteredvalue,key:Math.random().toString()}]);
         setenteredvalue('');
   }
   return (
-    // <View style={styles.outercontainer}>
-    //   <View style={styles.inputbox}>
-    //     <TextInput style={styles.textbox} placeholder='Enter your goals'  value={enteredvalue} onChangeText={goalipt}/>
-    //     <Button title='Add Goal' onPress={setgoal}/>
-    //   </View>  
-    //   <View style={styles.goallist}>
-    //      <FlatList data={goals} renderItem={({item})=><View style={styles.goalitem}><Text style={styles.goaltext}>{item}</Text></View>}/>
-    // </View>
-    // </View>
-    <View style={styles.container}>
-          <View style={styles.circle}>
-              
-          </View>
-         <View style={styles.box}>
-          
-          </View> 
+    <View style={styles.outercontainer}>
+           <View style={styles.inputbox}>
+                         <TextInput style={styles.textbox} placeholder='Enter your goals'  value={enteredvalue} onChangeText={goalipt}/>
+                         <Button title='Add Goal' onPress={setgoal}/>
+          </View>  
+
+       <View style={styles.goallist}>
+               <FlatList data={goals} keyExtractor={(item,index)=>{return item.key}} renderItem={({item})=><View style={styles.goalitem}><Text style={styles.goaltext}>{item.text}</Text></View>}/>
+        </View>
+ 
     </View>
-  );
+   );
 }
 
 const styles = StyleSheet.create({
-  // outercontainer:{
-  //   flex:1,
-  //   paddingTop:50,
-  //   paddingHorizontal:16,
-    
-  // },
-  // inputbox:{
-  //   flex:1,
-  //   flexDirection:'row',
-  //   justifyContent:'space-between',
-  //   alignItems:'center',
-  //   marginBottom:24,
-  //   paddingRight:10,
-  //   borderBottomWidth:1,
-  //   borderBottomColor:'silver',
-  // },
-  // textbox:{
-  //   borderWidth:1,
-  //   borderColor:'silver',
-  //   padding:8,
-  //   width:'75%',
-  //   marginRight:8
-  // }
-  // ,
-  // goallist:{
-  //   flex:3,
-  //   paddingTop:10,
-  // },
-  // goalitem:{
-  //   margin:8,
-  //   padding:10,
-  //   borderWidth:2,
-  //   borderColor:'silver',
-  //   borderRadius:10,
-  //   backgroundColor:'red',
-  //   shadowColor:'white',
-  // },
-  // goaltext:{
-  //   color:'black',
-  // }
-  container:{
-    backgroundColor:'ffffff',
+  outercontainer:{
     flex:1,
+    paddingTop:50,
+    paddingHorizontal:16,
+    
+  },
+  inputbox:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'space-between',
     alignItems:'center',
-    justifyContent:'center',
+    marginBottom:24,
+    paddingRight:10,
+    borderBottomWidth:1,
+    borderBottomColor:'silver',
   },
-  text:{
-    fontSize:30,
-      color:'white',
+  textbox:{
+    borderWidth:1,
+    borderColor:'silver',
+    padding:8,
+    width:'75%',
+    marginRight:8
+  }
+  ,
+  goallist:{
+    flex:3,
+    paddingTop:10,
   },
-  circle:{
-    borderColor:'black',
-    borderWidth:6,
-    height:200,
-    width:200,
-    borderRadius:100,
+  goalitem:{
+    margin:8,
+    padding:10,
+    borderWidth:2,
+    borderColor:'silver',
+    borderRadius:10,
+    backgroundColor:'skyblue',
+    shadowColor:'white',
   },
-  box:{
-    position:'absolute',   
-    borderWidth:6,
-    height:200,
-    width:200,
-    borderColor:'red',
-   }
+  goaltext:{
+    color:'black',
+  }
 });
